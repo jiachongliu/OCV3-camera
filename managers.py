5dc463659fc5e72e43f2b83e8b706676ebffecd7
jiachongliu <jcl314159@gmail.com>
2020-05-03 21:15:16 +0800

    continue
    add some file

--- managers.py
+++ managers.py
@@ -18,7 +18,7 @@ class CaptureManager(object):
         self._videoEncoding       = None
         self._videoWriter         = None
         self._startTime           = None
-        self._framesElapsed       = long(0)
+        self._framesElapsed       = 0
         self._fpsEstimate         = None
 
 
@@ -45,4 +45,101 @@ class CaptureManager(object):
     @property
     def isWritingVideo(self):
         return self._videoFilename is not None
+
+    def enterFrame(self):
+        assert not self._enteredFrame, \
+                'previous enterFrame() had no matching exitFrame()'
+
+        if self._capture is not None :
+            self._enteredFrame = self._capture.grab()
+
+    def exitFrame(self):
+        if self.frame is None:
+            self._enteredFrame = False
+            return 
+
+        if self._framesElapsed == 0:
+            self._startTime = time.time()
+        else:
+            timeElapsed = time.time() - self._startTime
+            self._fpsEstimate = self._framesElapsed / timeElapsed
+            self._framesElapsed += 1
+
+
+        if self.previewWindowManager is not None:
+            if self.shouldMirrorPreview:
+                mirroredFrame = np.fliplr(self._frame).copy()
+                self.previewWindowManager.show(mirroredFrame)
+            else:
+                self.previewWindowManager.show(self._frame)
+                
+        if self.isWritingImage:
+            cv2.imwrite(self._imageFilename, self._frame)
+            self._imageFilename = None
+            
+        self._writeVideoFrame()
+        
+        self._frame = None
+        self._enteredFrame = False
+        
+    def writeImage(self, filename):
+        self._imageFilename = filename
+
+    def startWritingVideo(self, filename, encoding = cv2.VideoWriter_fourcc('I', '4', '2', '0')):
+        self._videoFilename = filename
+        self._videoEncoding = encoding
+
+    def stopWritingVideo(self):
+        self._videoFilename = None
+        self._videoEncoding = None
+        self._videoWriter   = None
+
+    def _writeVideoFrame(self):
+        if not self.isWritingVideo:
+            return 
+
+        if self._videoWriter is None:
+            fps = self._capture.get(cv2.CAP_PROP_FPS)
+            if fps == 0.0:
+                if self._framesElapsed < 20:
+                    return 
+                else:
+                    fps = self._fpsEstimate
+            size = (int(self._capture.get(
+                        cv2.CAP_PROP_FRAME_WIDTH)),
+                    int(self._capture.get(
+                        cv2.CAP_PROP_FRAME_HEIGHT))
+                    )
+            self._videoWriter = cv2.VideoWriter(
+                    self._videoFilename, self._videoEncoding,
+                    fps, size)
+        self._videoWriter.write(self._frame)
+
+
+class WindowManager(object):
+    
+    def __init__(self, windowName, keypressCallback = None):
+        self.keypressCallback = keypressCallback
+        self._windowName      = windowName
+        self._isWindowCreated = False
+
+    @property
+    def isWindowCreated(self):
+        return self._isWindowCreated
+
+    def createWindow(self):
+        cv2.namedWindow(self._windowName)
+        self._isWindowCreated = True
+
+    def show(self, frame):
+        cv2.destroyWindow(self._windowName)
+        self._isWindowCreated = False
+
+    def processEvents(self):
+        keycode = cv2.waitKey(1)
+        if self.keypressCallback is not None and keycode != -1:
+            keycode &= 0xFF
+            self.keypressCallback(keycode)
+
+
 
